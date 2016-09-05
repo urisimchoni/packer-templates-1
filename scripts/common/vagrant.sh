1a79ec872fdd5d1c6c6bb392efb1ad5a78a0bc31
Uri Simchoni <uri@samba.org>
2016-09-05 07:13:46 +0300

    vagrant: add comment about keypair
    That public key download looks creepy until you read
    the vagrant docs and realize this is the standard way for
    a box to be reachable by vagrant.

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -6,6 +6,9 @@ set -x
 date | sudo tee /etc/vagrant_box_build_time
 
 mkdir -p ~/.ssh
+
+# This is the vagrant insecure keypair - used by
+# vagrant by default to get into boxes.
 curl -fsSLo ~/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
 chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/authorized_keys
